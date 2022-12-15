c159be5d1dc50f08c21fdc82019a048074830e92
Khuseyn Khasiev <khuseyn-khasiev@mail.ru>
2022-12-15 18:50:07 +0300

    добавить возможность  удаления и зачеркивание дел

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import TodoList from './TodoList.js'
 
 const data1 = new Date(2021, 7, 19, 14, 5);
 const data2 = new Date(2021, 7, 19, 15, 23);
@@ -8,7 +9,7 @@ const initialData = [
     title: 'Изучить React',
     desc: 'Да поскорее',
     image: '',
-    done: true,
+    done: false,
     createAt: data1.toLocaleString(),
     key: data1.getTime()
   },
@@ -16,7 +17,7 @@ const initialData = [
     title: 'Написать первое React-приложение',
     desc: 'Список запланированнхы дел',
     image: '',
-    done: true,
+    done: false,
     createAt: data2.toLocaleString(),
     key: data2.getTime()
   }
@@ -25,8 +26,23 @@ const initialData = [
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.data = initialData;
+    this.state = {data: initialData}; 
+    this.setDone = this.setDone.bind(this);
+    this.delete = this.delete.bind(this);
   }
+  delete(key) {
+    const newData = this.state.data.filter((current) => current.key !== key);
+    this.setState((state) => ({data: newData}))
+  }
+  setDone(key){
+    console.log(key);
+    const deed = this.state.data.find((current) => current.key === key);
+    if (deed) {
+      deed.done = true;
+      this.setState((state) => ({}))
+    }
+  }
+
   render() {
     return (
       <div>
@@ -38,7 +54,7 @@ export default class App extends Component {
           </div>
         </nav>
         <main className='content px-6 mt-6'>
-          <h1>Todos</h1>
+          <TodoList list={this.state.data} setDone={this.setDone} delete={this.delete}/>
         </main>
       </div>
     )
